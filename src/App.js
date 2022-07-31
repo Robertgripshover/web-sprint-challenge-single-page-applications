0b3bd3df91f5d3d5a78983edf4a8ca952ec7904e
Robert Gripshover <robertgrips@gmail.com>
2022-07-31 10:56:44 -0400

    m

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ const App = () => {
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      <main>
+
+      </main>
+      <footer>
+        <p>
+        &copy copwwright 2022 Bloomtech Eats Inc or something
+        </p>
+      </footer>
+
     </>
   );
 };
