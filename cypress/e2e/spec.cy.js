f713608329c50f1843fffc717400d425788c5e2f
Robert Gripshover <robertgrips@gmail.com>
2022-08-26 13:48:02 -0400

    Here is my test commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,29 +1,29 @@
 
-describe('', () => {
-  beforeEach(() => {
-      cy.visit('http://localhost:3000/')
-  })
-  it('can visit the home page', () => {
-      cy.contains('Welcome, please customize a pizza!')
-  })
-  it('can order a pizza', () => {
-    const pizzaFormOrder = cy.get(`[data-test-id='goform']`)
-    pizzaFormOrder.click()
-    cy.contains('Pizza Order Form')
-    const submitBtn = () => cy.get(`[data-test-id='order-submit']`)
+// describe('', () => {
+//   beforeEach(() => {
+//       cy.visit('http://localhost:3000/')
+//   })
+//   it('can visit the home page', () => {
+//       cy.contains('Welcome, please customize a pizza!')
+//   })
+//   it('can order a pizza', () => {
+//     const pizzaFormOrder = cy.get(`[data-test-id='goform']`)
+//     pizzaFormOrder.click()
+//     cy.contains('Pizza Order Form')
+//     const submitBtn = () => cy.get(`[data-test-id='order-submit']`)
 
-    cy.get(`[data-test-id='order-name']`).type('pizzatest')
+//     cy.get(`[data-test-id='order-name']`).type('pizzatest')
 
-    cy.get(`[data-test-id='order-size']`).select('Small')
+//     cy.get(`[data-test-id='order-size']`).select('Small')
 
-    cy.get(`[data-test-id='order-feta']`).click()
+//     cy.get(`[data-test-id='order-feta']`).click()
 
-    cy.get(`[data-test-id='order-olives']`).click()
+//     cy.get(`[data-test-id='order-olives']`).click()
 
-    cy.get(`[data-test-id='order-peppers']`).click()
+//     cy.get(`[data-test-id='order-peppers']`).click()
 
-    cy.get(`[data-test-id='order-onions']`).click()
+//     cy.get(`[data-test-id='order-onions']`).click()
 
-  })
+//   })
 
-})
+// })
